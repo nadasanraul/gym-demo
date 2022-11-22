793f3a2251c29b695d491fa38d44ea0ad51b175e
Raul Nadasan <raul@orderchamp.com>
2022-11-22 14:19:57 +0100

    Tweaks of user migration and model

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,15 +27,5 @@ class User extends Model
      */
     protected $hidden = [
         'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
