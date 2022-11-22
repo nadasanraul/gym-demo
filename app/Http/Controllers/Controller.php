d54bace9ea245a9eca842069cf53565c7e10ae8d
Raul Nadasan <raul@orderchamp.com>
2022-11-22 14:21:09 +0100

    Added error response function on the base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
+use Throwable;
 
 class Controller extends BaseController
 {
     use ValidatesRequests;
+
+    protected function errorResponse(int $statusCode, Throwable $throwable, bool $debug = false): JsonResponse
+    {
+        $data = [
+            'message' => 'Something went wrong, please try again later',
+        ];
+
+        if ($debug) {
+            $data['debug'] = [
+                'message' => $throwable->getMessage(),
+                'file' => $throwable->getFile(),
+                'line' => $throwable->getLine(),
+                'trace' => $throwable->getTrace(),
+            ];
+        }
+
+        return response()->json($data, $statusCode);
+    }
 }
