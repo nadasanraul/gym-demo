385bc2872c16b5896a20e785a729e85906a54469
Raul Nadasan <raul@orderchamp.com>
2022-11-23 22:18:14 +0100

    Seeding dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Invoice;
+use App\Models\InvoiceLine;
+use App\Models\Membership;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        foreach (User::all() as $user) {
+            Membership::factory()->create(['user_id' => $user->id]);
+            $invoice = Invoice::factory()->create(['user_id' => $user->id]);
+            InvoiceLine::factory(10)->create(['invoice_id' => $invoice->id]);
+         }
     }
 }
